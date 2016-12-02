7cdd892d97514b103a00cac77eab06e4bd272f75
Tim Santeford <tsantef@gmail.com>
2016-12-02 10:16:27 -0800

    Dropping `space-infix-ops` rule for now

--- base.js
+++ base.js
@@ -74,7 +74,6 @@ module.exports = {
 		'space-before-blocks': [2, 'always'],
 		'space-before-function-paren': [2, 'never'],
 		'space-in-parens': [2, 'never'],
-		'space-infix-ops': 2,
 		'space-unary-ops': 2,
 		'template-curly-spacing': 2,
 
