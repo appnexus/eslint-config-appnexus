bbe283cd3d4541d21d893e2d1cb7d12213bc8794
crayen <crayen@appnexus.com>
2021-06-16 15:42:27 -0700

    Adding __html as valid entry for  @typescript-eslint/naming-convention

--- test/typescript/typescript.spec.js
+++ test/typescript/typescript.spec.js
@@ -5,22 +5,39 @@ const cli = new CLIEngine({
 	configFile: './typescript.js',
 });
 
-describe.skip('Typescript', () => {
+describe('Typescript', () => {
 	it('should catch lint', () => {
 		expect.assertions(1);
 
 		const report = cli.executeOnFiles(['test/typescript/test-source.ts']);
 
 		const errors = _.map(report.results[0].messages, (message) => {
-			return _.pick(message, 'ruleId', 'severity');
+			return _.pick(message, 'ruleId', 'severity', 'message');
 		});
 
 		expect(errors).toEqual([
 			{
+				message: 'Type number trivially inferred from a number literal, remove type annotation.',
 				ruleId: '@typescript-eslint/no-inferrable-types',
 				severity: 2,
 			},
 			{
+				message: "'x' is assigned a value but never used.",
+				ruleId: '@typescript-eslint/no-unused-vars',
+				severity: 2,
+			},
+			{
+				message: "'htmlAttributes' is assigned a value but never used.",
+				ruleId: '@typescript-eslint/no-unused-vars',
+				severity: 2,
+			},
+			{
+				message: 'Interface name `TestValue` must match the RegExp: /^I[A-Z]/u',
+				ruleId: '@typescript-eslint/naming-convention',
+				severity: 2,
+			},
+			{
+				message: "'TestValue' is defined but never used.",
 				ruleId: '@typescript-eslint/no-unused-vars',
 				severity: 2,
 			},
